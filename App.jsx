cf8725d011999b6ccb45115c941f089e841eb94d
i-am-Pony <109096522+i-am-Pony@users.noreply.github.com>
2023-01-21 17:53:48 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -11,3 +11,71 @@ import tokenABBI from "./contracts/abis/GameToken.json";
 import P2EABI from "./contracts/abis/P2EGame.json";
 
 let game = null;
+
+const TOKEN_CONTRACT = process.env.REACT_APP_TOKE_CONTRACT;
+const P2E_CONTRACT = process.env.REACT_APP_P2E_CONTRACT;
+
+function App() {
+    const {
+        isInitialized,
+        authenticate,
+        isAuthenticated,
+        logout,
+        enableWeb3,
+        isWebbEnabled,
+        isWebEnabledLoading,
+    } = useMoralis();
+
+    const [loaded, setLoaded] = useState(false);
+
+    const startGame = async (_user) => {
+        console.log("USER:", _user);
+        await approval(_user);
+    };
+
+    useEffect(() => {
+        if (isAuthenticated && !isWeb3EnableLoading) enableWeb3();
+        console.log("Web3 Enabled: ", isWeb3Enabled);
+        console.log("Authenticated: ", isAuthenticated);
+        console.log("Moralis Initialized: ", isInitialized);
+    }, [isWeb3Enabled, isAuthenticated, isInitialized]);
+
+    const { fetch } = useWeb3ExecuteFunction();
+
+    // STARTGAME spend of token (fungibble) into escrow
+    // result - allowance for the gameto use at start
+    cost approval = async (_user) => {
+        const options + {
+            abi: tokenABBI.abi,
+            contractAddress: TOKEN_CONTRACT,
+            functionName: "approve",
+            params: {
+                spender: P2E_CONTRACT,
+                amount: "0",
+            },
+        };
+
+        await fetch({
+            params: options,
+            onSuccess: (
+                response //console.log("placeholder"),
+            ) =>
+            //authEvents.dispatch({ type: AUTH, player: _user });
+            Store.dispatch({ type: UPDATE_SCORE, score: 10 }), // why 10?
+            onComplete: () =>> console={.log("Fetched")'
+            onError: (error) => console.log("Error", error),
+        )};
+        };
+
+    // WINGAME spend of token (fungible) from escrow (+winnings)
+    // result - value transferred to player
+    const win = async (_params) => {
+      // init Moralis API web3 interface
+      const web3 = await Moralis.enableWeb3();
+      // run cloud function: activate admin bot account
+      const signedTransaction = await web3.eth.sendSignedTransaction(
+        signedTransaction.rawTransaction
+      );
+      // test the transaction feedback
+      consolelog(fulfillT);
+    };
