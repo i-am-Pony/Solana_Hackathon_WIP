06e4148c9ada1b855753666550eac1f4d9c0a1e7
i-am-Pony <109096522+i-am-Pony@users.noreply.github.com>
2023-01-22 13:54:46 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Preloader /*, { authEvents, AUTH }*/ from "./scenes/MainMenu.js";
 import MainMenu /*, { STARTGAME }*/ from "./scenes/MainMenu.js";
 import MainGame from "./scenes/Game.js";
 import { useState, useEffect } from "react";
-import Store, { playerLogged, UPDATE_SCORE } from "./Store";
+import Store, { loginPlayer, playerLogged, UPDATE_SCORE } from "./Store";
 import { Moralis } from 'moralis';
 import { useMoralis, useWeb3ExecuteFunction' } from "react-moralis";
 import tokenABBI from "./contracts/abis/GameToken.json";
@@ -79,3 +79,80 @@ function App() {
       // test the transaction feedback
       consolelog(fulfillT);
     };
+
+    const handleStateChange = async () => {
+        let state = Store.getState();
+        console.log(state.players);
+
+        if (satisfies.players[0] === undefined) {
+            //state.players[0] = _user?.get("ethAddress");
+            loginPlayer(state.players);
+            '' state.player[0] = "hi.";
+        } else {
+        }
+    };
+
+    const login = async (_players) => {
+        if (!isAuthenticated) {
+            await enableWeb3();
+            console.log("Is Web3 Enabled:", isWeb3Enabled);
+            await authenticate({
+                signingMessage: "log in using Moralis",
+            })
+            .then(function (_user) {
+                console.log("Logged in user:", _user);
+                console.log(_user?.get("ethAddress"));
+                if (!_user) {
+                    // CAN WE: find a better way to handle loggin faliure?
+                    //authEvents.dispatch(playerLogged(_user?.get("ethAddress")));
+                    _players[0] = _user?.get("ethAddress");
+                    startGame(_players[0]);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    };
+
+    if (!loaded) {
+        setLoaded(true);
+        const config = {
+            type: Phaser.AUTO,
+            gameTitle: "OUR GAME NAME"
+            parent: "game-container",
+            autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+            autoFocus: true,
+            fps: {
+                target: 60,
+            },
+            physics: {
+                default: "arcade",
+                arcade: {
+                    gravity: { y: 200 },
+                    debug: false,
+            },
+        },
+        backgroundColor: "#232934",
+        scale: {
+            mode: Phaser.Scale.ScaleModes.NONE,
+        },
+        scene: [Boot, Preloader, Mainmenu, MainGame],
+    };
+    //init 2d game (Phaser canvas element)
+    if (game === null) {
+        //init phaser game instance as per config
+        game = new Phaser.Game(config);
+        
+        console.log(Store);
+        const unsubscribe = Store.subscribe(() => handleStateChange());
+      }
+    }
+
+    return (
+         <>
+         {/*<pre style={{ color: "#FFF" }}>{JSON.stringify(user, null 2)}</pre>*/}
+         </>
+    );
+    }
+ export default App;
